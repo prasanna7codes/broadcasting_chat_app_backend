e99c838bed4f7258c99758242db1f1a7a5653ccd
Prasanna Sahoo <166421920+prasanna7codes@users.noreply.github.com>
2025-06-30 21:19:34 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { DataModel } from "./db";
 const PORT = process.env.PORT || 8080;
 
 
-const DB_URI = "mongodb+srv://prasannasahoo0806:pua5dRtvJRTYxvGm@cluster0.lx9jyi5.mongodb.net/ChatData";
+const DB_URI = process.env.MONGO_DB;
 
 async function StartServer() {
   try {
